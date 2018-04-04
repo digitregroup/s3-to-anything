ed00151f6862815c9bd7d7a12ab642ee3bf0b7b1
Julien <julien.bouisset@digitregroup.com>
2018-04-04 17:08:34 +0200

    Made requirable

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-export {default} from './src/s3-to-anything'
+module.exports                = require('./src/s3-to-anything');
+module.exports.S3ToAnything   = require('./src/s3-to-anything');
+module.exports.S3Client       = require('./src/s3-client');
+module.exports.S3EventParser  = require('./src/s3-event-parser');
+module.exports.handleJoiError = require('./src/handle-joi-error');
